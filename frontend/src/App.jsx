53295a1d2b62f9820d68344e07d399499f9b22ae
Manu Pranav <manupranav62@gmail.com>
2023-05-11 18:18:05 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Home, CreatePost } from "./pages";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+const App = () => {
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  )
+};
+
+export default App;
