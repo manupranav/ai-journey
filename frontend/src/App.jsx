5e514f7046b3a06c138fdfdb67c47c62579faa21
Manu Pranav <manupranav62@gmail.com>
2023-05-15 08:54:01 +0530

    updated frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
-import { logo } from "./assets";
-import { Home, CreatePost } from "./pages";
+import { logo } from './assets';
+import { Home, CreatePost } from './page';
 
 const App = () => (
   <BrowserRouter>
@@ -11,12 +11,7 @@ const App = () => (
         <img src={logo} alt="logo" className="w-28 object-contain" />
       </Link>
 
-      <Link
-        to="/create-post"
-        className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
-      >
-        Create
-      </Link>
+      <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
