dc9fbd022d368f82d5a6cf6e50478c1af0548f46
Manu Pranav <manupranav62@gmail.com>
2023-05-15 08:49:36 +0530

    added backend

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./mongodb/connect.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+// routes
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello friend!");
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("Server running on http://127.0.0.1:8080")
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+startServer();
